7a43eb12825a4d5d589a2a728653bb92f41c48e6
nilokc <nilarolat@gmail.com>
2025-01-25 20:16:46 +0100

    Fixed websocket connection issue, Test case implemented

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -1,27 +1,70 @@
 package stepDefinitions;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.Random;
 
 public class Contact_Us_Steps {
 
+    private WebDriver driver;
+
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+    }
+
+
+    public static ChromeDriver getChromeDriver() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--remote-allow-origins=*");
+        return new ChromeDriver(chromeOptions);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
     @Given("I access the webdriver university contact us page")
     public void i_access_the_webdriver_university_contact_us_page() {
-        System.out.println("Test1");
+        driver.get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+       // System.out.println("Test1");
        // throw new io.cucumber.java.PendingException();
     }
 
     @When("I enter a unique first name")
     public void i_enter_a_unique_first_name() {
-        System.out.println("Test2");
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys("AuroFN" + generateRandomNumber(5));
+      //  System.out.println("Test2");
       // throw new io.cucumber.java.PendingException();
     }
 
+    //here adding a random name generator
     @And("I enter a unique last name")
     public void i_enter_a_unique_last_name() {
-        System.out.println("Test3");
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys("AutoFN" + generateRandomNumber(5));
+      //  System.out.println("Test3");
      //   throw new io.cucumber.java.PendingException();
+    }
+
+    public String generateRandomNumber(int lenght) {
+        return RandomStringUtils.randomNumeric(lenght);
     }
 
     @And("I enter a unique email address")
@@ -47,4 +90,8 @@ public class Contact_Us_Steps {
         System.out.println("Test7");
       //  throw new io.cucumber.java.PendingException();
     }
+
+
+
+
 }
